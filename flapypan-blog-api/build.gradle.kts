dac7087674d47232c23dccaf5b5222e99edbd9a7
FlapyPan <flapypan@gmail.com>
2023-07-31 00:34:06 +0800

    去除graalvm支持

--- flapypan-blog-api/build.gradle.kts
+++ flapypan-blog-api/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.2"
-    id("org.graalvm.buildtools.native") version "0.9.23"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
     kotlin("plugin.jpa") version "1.8.22"
