bfb06c928d10a48335a98c8fbc813d1ba0ffb2b4
FlapyPan <flapypan@gmail.com>
2023-09-13 12:23:48 +0800

    修复第一次加载不显示访问量问题

--- src/store/access.js
+++ src/store/access.js
@@ -5,7 +5,7 @@ import { ref } from 'vue'
 export const useAccessStore = defineStore('access', () => {
   const baseInfo = ref({ all: 0, today: 0 })
   const getBaseInfo = () => api(`/access`).then((info) => baseInfo.value = info)
-  setInterval(getBaseInfo, 5000)
+  getBaseInfo().then(() => setInterval(getBaseInfo, 5000))
 
   return { baseInfo }
 })
