5066ac986469a63f6dbb70cf28e2e6f7de266b0c
FlapyPan <flapypan@gmail.com>
2023-11-18 15:41:31 +0800

    修改认证中间件逻辑

--- server/api/auth/user.get.ts
+++ server/api/auth/user.get.ts
@@ -1,4 +1,4 @@
 // 检查登录状态
-export default eventHandler(async (event) => {
+export default eventHandler((event) => {
   return auth(event)
 })
--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,7 +1,13 @@
 export default eventHandler((event) => {
-  const method = getMethod(event)
-  // 不认证 get 请求
-  if (method !== 'GET') {
+  const { method, path } = event
+  do {
+    // 不认证非 api 请求
+    if (!path.startsWith('/api')) break
+    // 不认证 get 请求
+    if (method === 'GET') break
+    // 不认证登录请求
+    if (path.startsWith('/api/auth/login')) break
+
     event.context.user = auth(event)
-  }
+  } while (false)
 })
