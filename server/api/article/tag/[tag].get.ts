ace3256e247a9ff4ac24ab49caecfa6d636b96fd
FlapyPan <flapypan@gmail.com>
2023-11-21 10:51:45 +0800

    修复标签查找文章为空问题

--- server/api/article/tag/[tag].get.ts
+++ server/api/article/tag/[tag].get.ts
@@ -5,5 +5,7 @@ export default eventHandler(async (event) => {
   if (!tag) {
     throw createError({ statusCode: 404, message: '不存在的标签' })
   }
-  return getArticleListByTag(tag)
+  // 浏览器会将路径进行编码，这里进行解码
+  const decodedTag = decodeURI(tag)
+  return getArticleListByTag(decodedTag)
 })
