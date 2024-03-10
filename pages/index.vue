ec9b44c44308b4ef7bedba739be6681c2173ff35
FlapyPan <flapypan@gmail.com>
2024-03-10 18:59:47 +0800

    优化主页效果

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
+import { useIntervalFn, useParallax } from '@vueuse/core'
+import type { CSSProperties } from 'vue'
 import type { AccessData, ArticleWithoutContent } from '~/types/api'
 import { useSettingStore } from '~/store'
 
 const settingStore = useSettingStore()
+
+const heroElement = shallowRef<HTMLDivElement>()
+const { tilt, roll, source } = useParallax(heroElement)
+const heroEleStyle = computed<CSSProperties>(() => {
+  const ration = source.value === 'deviceOrientation' ? 20 : 10
+  return {
+    transform: `rotateX(${roll.value * ration}deg) rotateY(${tilt.value * ration}deg)`,
+  }
+})
 
 /// region 访问量和其他数据
 const { data: accessData } = await useLazyAsyncData(
@@ -32,22 +43,30 @@ async function getArticleData() {
 /// region 随机一言
 const hitoko = shallowRef('')
 
-async function fetchHitokoto(enable: boolean) {
-  if (import.meta.browser) {
-    if (!enable) return
-    try {
-      const { hitokoto: text } = await $fetch<{ hitokoto: string }>(
-        'https://v1.hitokoto.cn?c=a&c=c&c=d&c=j&c=k',
-      )
-      hitoko.value = text
-    } catch (e) {
-      console.error(e)
-      hitoko.value = '加载失败...'
-    }
+async function fetchHitokoto() {
+  try {
+    const { hitokoto: text } = await $fetch<{ hitokoto: string }>(
+      'https://v1.hitokoto.cn?c=a&c=c&c=d&c=j&c=k',
+    )
+    hitoko.value = text
+  } catch (e) {
+    console.error(e)
+    hitoko.value = '加载失败...'
   }
 }
 
-onMounted(() => watch(() => settingStore.setting.hitoko, fetchHitokoto, { immediate: true }))
+const { pause: pauseFetchHitokoto, resume: resumeFetchHitokoto } = useIntervalFn(fetchHitokoto, 10000)
+onMounted(() => watch(() => settingStore.setting.hitoko, (value) => {
+  if (value) {
+    if (!hitoko.value) {
+      fetchHitokoto().then(resumeFetchHitokoto)
+    } else {
+      resumeFetchHitokoto()
+    }
+  } else {
+    pauseFetchHitokoto()
+  }
+}, { immediate: true }))
 /// endregion 随机一言
 
 const title = `${settingStore.setting.name} - ${settingStore.setting.siteTitle ?? '博客'}`
@@ -84,52 +103,55 @@ useSeoMeta(meta)
       </Teleport>
     </ClientOnly>
     <section
-      class="flex h-screen w-full flex-col-reverse items-center justify-center gap-16 text-center md:flex-row md:text-left lg:gap-52"
+      ref="heroElement"
+      style="perspective: 200px"
+      class="flex h-screen w-full items-center justify-center p-12"
     >
-      <div>
-        <h1 class="text-3xl text-zinc-700 dark:text-zinc-100 sm:text-4xl lg:text-5xl">
-          {{ settingStore.setting.siteTitle }}
-        </h1>
-        <div class="text-zinc-600 dark:text-zinc-400">
-          <p class="mt-8 flex items-center justify-center gap-4 text-2xl md:justify-start md:gap-2">
-            <a
-              :href="`mailto:${settingStore.setting.email}`"
-              class="flex items-center"
-              title="邮箱联系我"
-            >
-              <Icon name="mingcute:at-line" />
-            </a>
-            <a
-              :href="`https://github.com/${settingStore.setting.name}`"
-              class="flex items-center"
-              title="github"
-            >
-              <Icon name="mdi:github" />
-            </a>
-          </p>
-          <div
-            v-if="settingStore.setting.hitoko"
-            v-auto-animate
-            class="mt-4 text-center text-sm md:text-left"
-          >
-            <p v-if="hitoko">
-              一言：{{ hitoko }}
+      <div
+        :style="heroEleStyle"
+        class="flex flex-col-reverse items-center justify-center gap-16 text-center will-change-transform md:flex-row md:text-left lg:gap-52"
+      >
+        <div>
+          <h1 class="text-3xl text-zinc-700 dark:text-zinc-100 sm:text-4xl lg:text-5xl">
+            {{ settingStore.setting.siteTitle }}
+          </h1>
+          <div class="text-zinc-600 dark:text-zinc-400">
+            <p class="mt-6 flex items-center justify-center gap-4 text-2xl md:justify-start md:gap-2">
+              <a
+                :href="`mailto:${settingStore.setting.email}`"
+                class="flex items-center"
+                title="邮箱联系我"
+              >
+                <Icon name="mingcute:at-line" />
+              </a>
+              <a
+                :href="`https://github.com/${settingStore.setting.name}`"
+                class="flex items-center"
+                title="github"
+              >
+                <Icon name="mdi:github" />
+              </a>
             </p>
-            <p v-else>
-              一言加载中...
+            <div v-if="settingStore.setting.hitoko" class="mt-4 text-center text-sm md:w-72 md:text-left">
+              <p v-if="hitoko">
+                {{ hitoko }}
+              </p>
+              <p v-else>
+                加载中...
+              </p>
+            </div>
+            <p v-else class="mt-4 text-center text-sm md:text-left">
+              {{ settingStore.setting.info }}
+              <span class="animate-ping">_</span>
             </p>
           </div>
-          <p v-else class="mt-4 text-center text-sm md:text-left">
-            {{ settingStore.setting.info }}
-            <span class="animate-ping">_</span>
-          </p>
         </div>
+        <img
+          :src="settingStore.setting.avatar"
+          alt=""
+          class="size-32 rounded-full md:size-40 lg:size-52"
+        >
       </div>
-      <img
-        :src="settingStore.setting.avatar"
-        alt=""
-        class="size-32 rounded-full md:size-40 lg:size-52"
-      >
     </section>
     <section class="page">
       <h3 class="flex items-center">
